6ff3ef8b871935284bb10a5984493a4ea4b87f8b
Benjamin King <king7532@mac.com>
2019-09-07 15:50:42 -0400

    setup-macports action ready for testing

---
+++ setup-macports/lib/main.js
@@ -0,0 +1,72 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core = __importStar(require("@actions/core"));
+const io = __importStar(require("@actions/io"));
+const exec = __importStar(require("@actions/exec"));
+const tc = __importStar(require("@actions/tool-cache"));
+function getMacPorts(versionSpec = "2.5.4") {
+    return __awaiter(this, void 0, void 0, function* () {
+        // check cache
+        let toolPath;
+        toolPath = tc.find("macports", versionSpec);
+        // If not found in cache, download
+        if (!toolPath) {
+            // Download, install, cache
+            core.debug("MacPorts not found in tool-cache");
+            let filename = "MacPorts-" + versionSpec + "-10.14-Mojave.pkg";
+            let downloadUrl = "https://distfiles.macports.org/MacPorts/" + filename;
+            let downloadPath;
+            try {
+                downloadPath = yield tc.downloadTool(downloadUrl);
+            }
+            catch (err) {
+                core.setFailed("Could not download MacPorts: " + err);
+            }
+            let exitCode = yield exec.exec("sudo /usr/sbin/installer", [
+                "-pkg",
+                filename,
+                "-target",
+                "/"
+            ]);
+            if (exitCode != 0) {
+                core.setFailed(`Could not install MacPorts. Exit code = ${exitCode}`);
+            }
+        }
+        yield tc.cacheDir("/opt/local", "macports", versionSpec);
+        core.addPath("/opt/local/bin:/opt/local/sbin");
+    });
+}
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            getMacPorts();
+            if (core.getInput("UseHostCMake")) {
+                const cmakePath = yield io.which("cmake");
+                yield exec.exec("sudo ln", ["-s", cmakePath, "/opt/local/bin/cmake"]);
+            }
+        }
+        catch (error) {
+            core.setFailed(error.message);
+        }
+    });
+}
+/////// start
+if (process.platform != "darwin") {
+    core.setFailed("This action *only* runs on macOS");
+}
+run();
--- setup-macports/src/main.ts
+++ setup-macports/src/main.ts
@@ -1,12 +1,59 @@
-import * as core from '@actions/core';
+import * as core from "@actions/core";
+import * as io from "@actions/io";
+import * as exec from "@actions/exec";
+import * as tc from "@actions/tool-cache";
+
+async function getMacPorts(versionSpec: string = "2.5.4") {
+  // check cache
+  let toolPath: String;
+  toolPath = tc.find("macports", versionSpec);
+
+  // If not found in cache, download
+  if (!toolPath) {
+    // Download, install, cache
+    core.debug("MacPorts not found in tool-cache");
+
+    let filename = "MacPorts-" + versionSpec + "-10.14-Mojave.pkg";
+    let downloadUrl = "https://distfiles.macports.org/MacPorts/" + filename;
+
+    let downloadPath: string;
+    try {
+      downloadPath = await tc.downloadTool(downloadUrl);
+    } catch (err) {
+      core.setFailed("Could not download MacPorts: " + err);
+    }
+    let exitCode = await exec.exec("sudo /usr/sbin/installer", [
+      "-pkg",
+      filename,
+      "-target",
+      "/"
+    ]);
+    if (exitCode != 0) {
+      core.setFailed(`Could not install MacPorts. Exit code = ${exitCode}`);
+    }
+  }
+
+  await tc.cacheDir("/opt/local", "macports", versionSpec);
+  core.addPath("/opt/local/bin:/opt/local/sbin");
+}
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    getMacPorts();
+
+    if (core.getInput("UseHostCMake")) {
+      const cmakePath = await io.which("cmake");
+      await exec.exec("sudo ln", ["-s", cmakePath, "/opt/local/bin/cmake"]);
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+
+/////// start
+
+if (process.platform != "darwin") {
+  core.setFailed("This action *only* runs on macOS");
 }
 
 run();
